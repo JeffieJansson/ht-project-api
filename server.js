d00cddec87f23294abb297a24569cb0173403659
Jennifer Jansson <jenniferjansson92@gmail.com>
2026-01-20 15:20:29 +0100

    Add GET endpoints for thoughts and API documentation

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 import cors from "cors"
 import express from "express"
+import data from "./data.json" with { type: "json" }
+import listEndpoints from "express-list-endpoints";
+
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 9000
 const app = express()
 
 // Add middlewares to enable cors and json body parsing
@@ -13,7 +16,23 @@ app.use(express.json())
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!")
+  const endpoints = listEndpoints(app);
+
+  res.json([{
+    message: "Welcome to my Thoughts API",
+    endpoints: endpoints
+  }]);
+});
+
+
+app.get("/thoughts", (req, res) => {
+  res.json(data)
+})
+
+app.get("/thoughts/:id", (req, res) => {
+  const { id } = req.params
+  const thoughtsId = data.find((thoughts) => thoughts._id === id)
+  res.json(thoughtsId)
 })
 
 // Start the server
