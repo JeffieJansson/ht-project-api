a0175a72d0b8d639ddbd396040c799f2985fb45b
Jennifer Jansson <jenniferjansson92@gmail.com>
2026-01-21 11:48:55 +0100

    add filter of hearts to the endpoint which returns all thoughts

--- server.js
+++ server.js
@@ -4,17 +4,14 @@ import data from "./data.json" with { type: "json" }
 import listEndpoints from "express-list-endpoints";
 
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
 const port = process.env.PORT || 9000
 const app = express()
 
-// Add middlewares to enable cors and json body parsing
+
 app.use(cors())
 app.use(express.json())
 
-// Start defining your routes here
+// documentation of the API with express-list-endpoints
 app.get("/", (req, res) => {
   const endpoints = listEndpoints(app);
 
@@ -24,16 +21,34 @@ app.get("/", (req, res) => {
   }]);
 });
 
-
+// One endpoint to return a collection of results(GET all thoughts)
 app.get("/thoughts", (req, res) => {
-  res.json(data)
+  const { hearts } = req.query; // Get the hearts using query parameter
+  let filteredData = data;
+
+  // Filter hearts greater than or equal to the specified value to the endpoint which returns all thoughts
+  if (hearts) {
+    filteredData = filteredData.filter((thought) => {
+      return thought.hearts >= Number(hearts);
+    });
+  }
+
+  res.json(filteredData);
 })
 
+
+// One endpoint to return a single result  (GET thoughts by id)
 app.get("/thoughts/:id", (req, res) => {
   const { id } = req.params
   const thoughtsId = data.find((thoughts) => thoughts._id === id)
+
+  if(!thoughtsId) {
+      return res.status(404).json({ error: `Thought with id ${id} not found` });
+    }
   res.json(thoughtsId)
 })
+
+
 
 // Start the server
 app.listen(port, () => {
