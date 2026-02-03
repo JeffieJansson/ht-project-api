2aed55c56fc14457b3d1ea19615ed3201628141e
Jennifer Jansson <jenniferjansson92@gmail.com>
2026-02-03 13:03:35 +0100

    created new file and moved thought model to it

---
+++ models/Thought.js
@@ -0,0 +1,23 @@
+import mongoose, { Schema } from "mongoose";
+
+const ThoughtSchema = new mongoose.Schema({
+  message: {
+    type: String,
+    required: [true, "A Message is required"],
+    minlength: [5, "A Message must be at least 5 characters"],
+    maxlength: [140, "A Message cannot exceed 140 characters"],
+    trim: true,
+  },
+  hearts: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Thought = mongoose.model("Thought", ThoughtSchema);
+
+export default Thought;
