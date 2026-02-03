c00a591b884e076525d11f35d5fff339d69a19b7
Jennifer Jansson <jenniferjansson92@gmail.com>
2026-02-03 13:04:43 +0100

    created user model

---
+++ models/User.js
@@ -0,0 +1,23 @@
+import mongoose, { Schema } from "mongoose";
+import crypto from "crypto";
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  accessToken: {
+    type: String,
+    required: true,
+    default: () => crypto.randomBytes(128).toString("hex"),
+  },
+})
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
